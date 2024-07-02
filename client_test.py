ee93cf9343748a344ac2623d3725b6b5924f7947
rfk123 <rfkamali@gmail.com>
2024-07-02 12:23:46 -0700

    Removed the use of round() from client3.py in the getDataPoint method. Implemented unit tests for getDataPoint and getRatio.

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    datapoints = [
+      ('ABC', 120.48, 121.2, 120.84),
+      ('DEF', 117.87, 121.68, 119.775)
+    ]
+
+    for quote, datapoint in zip(quotes, datapoints):
+      self.assertEqual(getDataPoint(quote), datapoint)
+
+    print("[Test passed on getDataPoint: Calculate Price Test]")
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +24,34 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    datapoints = [
+      ('ABC', 120.48, 119.2, 119.84),
+      ('DEF', 117.87, 121.68, 119.775)
+    ]
 
+    for quote, datapoint in zip(quotes, datapoints):
+      self.assertEqual(getDataPoint(quote), datapoint)
+
+    print("[Test passed on getDataPoint: Calculate Price Test Bid Greater Than Ask]")
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    prices = {
+      'ABC': 127.89,
+      'DEF': 120.84
+    }
+    datapoint = prices['ABC'] / prices['DEF']
+    self.assertEqual(getRatio(prices['ABC'], prices['DEF']), datapoint)
+    print("[Test passed on getRatio: Calculate Ratio Test]")
 
+  def test_getRatio_calculateRatioZeroDivision(self):
+    prices = {
+      'ABC': 127.89,
+      'DEF': 00.00
+    }
 
+    self.assertEqual(getRatio(prices['ABC'], prices['DEF']), None)
+    print("[Test passed on getRatio: Calculate Ratio Test ZeroDivision]")
 
 if __name__ == '__main__':
     unittest.main()
